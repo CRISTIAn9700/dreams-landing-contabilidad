1e9b5e61825e1f938bf791a2ff172d10cda333d6
CRISTIAN9700 <ocampo.otalvaro.cristian@gmail.com>
2026-06-24 21:39:09 -0500

    Connect Supabase project config

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -1,4 +1,4 @@
 window.DREAMS_SUPABASE_CONFIG = {
-    url: '',
-    anonKey: ''
+    url: 'https://shtyaewoffmqpujwmfzg.supabase.co',
+    anonKey: 'sb_publishable_RsxSVSQACYWwFX91fJ16wA_YeOa2JsX'
 };
